ddb543640fc0c100a6ad100139e10001b0437620
Jeong Myeonggu <audrn6689@gmail.com>
2022-08-03 23:21:22 +0900

    Chore: checkstyle 추가 설정

--- livecommerce/src/test/java/com/flab/livecommerce/LivecommerceApplicationTests.java
+++ livecommerce/src/test/java/com/flab/livecommerce/LivecommerceApplicationTests.java
@@ -6,8 +6,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class LivecommerceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+
+    }
 
 }
