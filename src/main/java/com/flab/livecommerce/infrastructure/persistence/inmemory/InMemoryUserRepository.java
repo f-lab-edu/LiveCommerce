c4fbd8b16e218956ddebc8e4512d9f4d303fada6
DJ-archive <ydajeong7@gmail.com>
2022-08-13 23:51:30 +0900

    refactor: user 식별자 수정
    동시성 이슈 고려하여 AtomicLong으로 수정

--- src/main/java/com/flab/livecommerce/infrastructure/persistence/inmemory/InMemoryUserRepository.java
+++ src/main/java/com/flab/livecommerce/infrastructure/persistence/inmemory/InMemoryUserRepository.java
@@ -4,17 +4,20 @@ package com.flab.livecommerce.infrastructure.persistence.inmemory;
 import com.flab.livecommerce.domain.user.User;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
+@Slf4j
 public class InMemoryUserRepository {
 
     private static Map<Long, User> userMap = new ConcurrentHashMap<>();
-    private static Long sequence = 0L;
+    private static AtomicLong sequence = new AtomicLong();
 
     public User save(User user) {
-        user.setId(++sequence);
+        user.setId(sequence.incrementAndGet());
         userMap.put(user.getId(), user);
         return user;
     }
