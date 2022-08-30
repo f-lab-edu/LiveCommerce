00b4592e8ebb85d388f944a196c50608afb3dd98
Jeong Myeonggu <audrn6689@gmail.com>
2022-08-30 14:20:34 +0900

    refacotr: flyway 환경설정 수정

--- src/main/resources/db.migration/V1.0__create_user_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE user (
-                      id INT NOT NULL AUTO_INCREMENT,
-                      email VARCHAR(255),
-                      password VARCHAR(255),
-                      nickname VARCHAR(255),
-                      PRIMARY KEY (id)
-);
