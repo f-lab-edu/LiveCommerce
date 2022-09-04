304e245c586ad17a1659089c61d2951f69d9f859
Jeong Myeonggu <audrn6689@gmail.com>
2022-09-04 17:21:41 +0900

    refactor: mysqlRepository 등록
    test 코드 때문에 현재는 inMemory 사용

--- src/main/java/com/flab/livecommerce/infrastructure/user/UserRepositoryAdapter.java
+++ src/main/java/com/flab/livecommerce/infrastructure/user/UserRepositoryAdapter.java
@@ -3,15 +3,21 @@ package com.flab.livecommerce.infrastructure.user;
 import com.flab.livecommerce.domain.user.User;
 import com.flab.livecommerce.domain.user.UserRepository;
 import com.flab.livecommerce.infrastructure.user.persistence.inmemory.InMemoryUserRepository;
+import com.flab.livecommerce.infrastructure.user.persistence.mysql.MysqlUserRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class UserRepositoryAdapter implements UserRepository {
 
     private final InMemoryUserRepository inMemoryUserRepository;
+    private final MysqlUserRepository mysqlUserRepository;
 
-    public UserRepositoryAdapter(InMemoryUserRepository inMemoryUserRepository) {
+    public UserRepositoryAdapter(
+        InMemoryUserRepository inMemoryUserRepository,
+        MysqlUserRepository mysqlUserRepository
+    ) {
         this.inMemoryUserRepository = inMemoryUserRepository;
+        this.mysqlUserRepository = mysqlUserRepository;
     }
 
     @Override
