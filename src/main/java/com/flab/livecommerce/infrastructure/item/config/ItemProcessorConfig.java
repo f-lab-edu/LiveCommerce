1599ab3515fc41e46bb189b59d13d4311c7b3731
Jeong Myeonggu <audrn6689@gmail.com>
2022-09-17 17:53:43 +0900

    refactor: 동일한 bean 이름 수정 및 사용하지 않는 설정 삭제
    MybatisConfig 는 설정안해도 이미 자동으로 yml 파일에서 셋팅이 됨.

--- src/main/java/com/flab/livecommerce/infrastructure/item/config/ItemProcessorConfig.java
+++ src/main/java/com/flab/livecommerce/infrastructure/item/config/ItemProcessorConfig.java
@@ -1,17 +1,17 @@
 package com.flab.livecommerce.infrastructure.item.config;
 
-import com.flab.livecommerce.application.item.ItemAddProcessor;
+import com.flab.livecommerce.application.item.RegisterItemProcessor;
 import com.flab.livecommerce.domain.item.ItemRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class ProcessorConfig {
+public class ItemProcessorConfig {
 
     @Bean
-    public ItemAddProcessor productAddProcessor(
+    public RegisterItemProcessor registerItemProcessor(
         ItemRepository itemRepository
     ) {
-        return new ItemAddProcessor(itemRepository);
+        return new RegisterItemProcessor(itemRepository);
     }
 }
