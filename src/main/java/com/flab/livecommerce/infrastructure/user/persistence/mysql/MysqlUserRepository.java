d2267e597ac689e1449683b7f28e3e93b0f4a1ab
Jeong Myeonggu <audrn6689@gmail.com>
2022-09-20 15:06:31 +0900

    refactor: insert 문 수정
    id 반환하도록 구현했는데 user.set 을 사용해서 반환을 해야하는지 잘 모르겠습니다.

--- src/main/java/com/flab/livecommerce/infrastructure/user/persistence/mysql/MysqlUserRepository.java
+++ src/main/java/com/flab/livecommerce/infrastructure/user/persistence/mysql/MysqlUserRepository.java
@@ -6,28 +6,49 @@ import javax.sql.DataSource;
 import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class MysqlUserRepository {
 
     private JdbcTemplate template;
+    private final SimpleJdbcInsert jdbcInsert;
 
     public MysqlUserRepository(DataSource dataSource) {
         this.template = new JdbcTemplate(dataSource);
+        this.jdbcInsert = new SimpleJdbcInsert(dataSource)
+            .withTableName("user") // user 테이블에 삽입
+            .usingGeneratedKeyColumns("id"); // id 컬럼의 값을 key로 반환
     }
 
     public User save(User user) {
-        String sql = "insert into user(id, email, password, nickname) value(?, ?, ?, ?)";
-        template.update(
-            sql,
-            user.getId(),
-            user.getEmail(),
-            user.getPassword(),
-            user.getNickname()
-        );
+        insertUser(user);
 
         return user;
+    }
+
+    private Long insertUser(User user) {
+        SqlParameterSource parameterSource = new MapSqlParameterSource()
+            .addValue("email", user.getEmail())
+            .addValue("password", user.getPassword())
+            .addValue("nickname", user.getNickname());
+
+        //todo 해당 부분에서 user Id set??
+        return jdbcInsert.executeAndReturnKey(parameterSource).longValue();
+    }
+
+    private void updateUser(User user) {
+        String sql = "update user set email=?, password=?, nickname=? where id=?";
+        template.update(
+            sql,
+            user.getEmail(),
+            user.getPassword(),
+            user.getNickname(),
+            user.getId()
+        );
     }
 
     public User findByEmail(String email) {
