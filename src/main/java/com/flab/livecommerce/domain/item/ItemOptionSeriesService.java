0f78f3991edb7130b4dabb5cfed5c302ba2a43ce
Jeong Myeonggu <audrn6689@gmail.com>
2022-09-24 16:53:19 +0900

    feat: 아이템 옵션시리즈 인터페이스, 구현체 구현
    애플리케이션 레이어 영역의 복잡한 구현로직을 인프라영역에 옮기기 위함

--- src/main/java/com/flab/livecommerce/domain/item/ItemOptionSeriesService.java
+++ src/main/java/com/flab/livecommerce/domain/item/ItemOptionSeriesService.java
@@ -1,8 +1,9 @@
 package com.flab.livecommerce.domain.item;
 
+import com.flab.livecommerce.application.item.command.RegisterItemCommand;
 import java.util.List;
 
 public interface ItemOptionSeriesService {
 
-    List<ItemOptionGroup> save(Item item);
+    List<ItemOptionGroup> save(RegisterItemCommand command, Item item);
 }
