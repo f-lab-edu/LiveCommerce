0ddb27de4ed310fd6c3d8634520a36b11359f20e
Jeong Myeonggu <audrn6689@gmail.com>
2022-09-24 16:59:00 +0900

    refactor: 옵션시리즈 등록 및 사용
    상품 옵션 저장시 복잡한 로직 제거 및 상품 등록 커맨드는 분리 함

--- src/main/java/com/flab/livecommerce/application/item/RegisterItemProcessor.java
+++ src/main/java/com/flab/livecommerce/application/item/RegisterItemProcessor.java
@@ -1,85 +1,27 @@
 package com.flab.livecommerce.application.item;
 
+import com.flab.livecommerce.application.item.command.RegisterItemCommand;
 import com.flab.livecommerce.domain.item.Item;
-import com.flab.livecommerce.domain.item.ItemOption;
-import com.flab.livecommerce.domain.item.ItemOptionGroup;
-import com.flab.livecommerce.domain.item.ItemOptionGroupRepository;
-import com.flab.livecommerce.domain.item.ItemOptionRepository;
+import com.flab.livecommerce.domain.item.ItemOptionSeriesService;
 import com.flab.livecommerce.domain.item.ItemRepository;
-import java.util.List;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 
 public class RegisterItemProcessor {
 
     private final ItemRepository itemRepository;
-    private final ItemOptionGroupRepository itemOptionGroupRepository;
-    private final ItemOptionRepository itemOptionRepository;
+    private final ItemOptionSeriesService itemOptionSeriesService;
 
     public RegisterItemProcessor(
         ItemRepository itemRepository,
-        ItemOptionGroupRepository itemOptionGroupRepository,
-        ItemOptionRepository itemOptionRepository
+        ItemOptionSeriesService itemOptionSeriesService
     ) {
         this.itemRepository = itemRepository;
-        this.itemOptionGroupRepository = itemOptionGroupRepository;
-        this.itemOptionRepository = itemOptionRepository;
+        this.itemOptionSeriesService = itemOptionSeriesService;
     }
 
     public Item execute(RegisterItemCommand command) {
         var item = itemRepository.save(command.toEntity());
-
-        command.getItemOptionGroup().forEach(
-            requestItemOptionGroup -> {
-                var optionGroup = ItemOptionGroup.builder()
-                    .itemId(item.getId())
-                    .ordering(requestItemOptionGroup.getOrdering())
-                    .name(requestItemOptionGroup.getName())
-                    .basic(requestItemOptionGroup.isBasic())
-                    .exclusive(requestItemOptionGroup.isExclusive())
-                    .minimumChoice(requestItemOptionGroup.getMinimumChoice())
-                    .maximumChoice(requestItemOptionGroup.getMaximumChoice())
-                    .build();
-                var itemOptionGroup = itemOptionGroupRepository.save(optionGroup);
-
-                itemOptionGroup.getItemOptions().forEach(
-                    requestItemOption -> {
-                        var option = ItemOption.builder()
-                            .itemOptionGroupId(itemOptionGroup.getItemId())
-                            .name(requestItemOption.getName())
-                            .ordering(requestItemOption.getOrdering())
-                            .price(requestItemOption.getPrice())
-                            .build();
-                        itemOptionRepository.save(option);
-                    }
-                );
-            }
-        );
+        itemOptionSeriesService.save(command, item);
 
         return item;
-    }
-
-    @Getter
-    @AllArgsConstructor
-    public static class RegisterItemCommand {
-
-        private String name;
-        private Integer price;
-        private Integer salesPrice;
-        private String description;
-        private Integer stockQuantity;
-        private int modelNumber;
-        private List<ItemOptionGroup> itemOptionGroup;
-
-        public Item toEntity() {
-            return Item.builder()
-                .name(name)
-                .description(description)
-                .price(price)
-                .salesPrice(salesPrice)
-                .stockQuantity(stockQuantity)
-                .modelNumber(modelNumber)
-                .build();
-        }
     }
 }
--- src/main/java/com/flab/livecommerce/infrastructure/item/config/ItemProcessorConfig.java
+++ src/main/java/com/flab/livecommerce/infrastructure/item/config/ItemProcessorConfig.java
@@ -3,7 +3,9 @@ package com.flab.livecommerce.infrastructure.item.config;
 import com.flab.livecommerce.application.item.RegisterItemProcessor;
 import com.flab.livecommerce.domain.item.ItemOptionGroupRepository;
 import com.flab.livecommerce.domain.item.ItemOptionRepository;
+import com.flab.livecommerce.domain.item.ItemOptionSeriesService;
 import com.flab.livecommerce.domain.item.ItemRepository;
+import com.flab.livecommerce.infrastructure.item.ItemOptionSeriesServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,11 +15,20 @@ public class ItemProcessorConfig {
     @Bean
     public RegisterItemProcessor registerItemProcessor(
         ItemRepository itemRepository,
-        ItemOptionGroupRepository itemOptionGroupRepository,
-        ItemOptionRepository itemOptionRepository
+        ItemOptionSeriesService itemOptionSeriesService
     ) {
         return new RegisterItemProcessor(
             itemRepository,
+            itemOptionSeriesService
+        );
+    }
+
+    @Bean
+    public ItemOptionSeriesService itemOptionSeriesService(
+        ItemOptionGroupRepository itemOptionGroupRepository,
+        ItemOptionRepository itemOptionRepository
+    ) {
+        return new ItemOptionSeriesServiceImpl(
             itemOptionGroupRepository,
             itemOptionRepository
         );
