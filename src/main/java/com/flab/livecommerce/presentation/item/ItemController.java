cd46391e7841ee9a32bda57f459a66421108d8b5
Jeong Myeonggu <audrn6689@gmail.com>
2022-09-28 15:20:38 +0900

    refactor: 아이템 등록, 이미지 등록 분리

--- src/main/java/com/flab/livecommerce/presentation/item/ItemController.java
+++ src/main/java/com/flab/livecommerce/presentation/item/ItemController.java
@@ -3,11 +3,11 @@ package com.flab.livecommerce.presentation.item;
 import com.flab.livecommerce.application.item.facade.ItemManager;
 import com.flab.livecommerce.common.response.CommonApiResponse;
 import com.flab.livecommerce.presentation.item.request.RegisterItemRequest;
-import java.io.IOException;
 import javax.validation.Valid;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,10 +26,19 @@ public class ItemController {
     // TODO 상세 이미지 리스트
     @PostMapping
     public CommonApiResponse registerItem(
+        @RequestBody @Valid RegisterItemRequest request
+    ) {
+        itemManager.register(request.toCommand());
+        return CommonApiResponse.success(null);
+    }
+
+    @PostMapping("image")
+    CommonApiResponse registerItemImage(
         @Valid @RequestPart("itemData") RegisterItemRequest request,
         @RequestPart("thumbnailUrl") MultipartFile thumbnailImg
-    ) throws IOException {
-        itemManager.register(request.toCommand(), thumbnailImg);
+    ) {
+        //todo item 등록과 분리
+        //itemManager.register(request.toCommand(), thumbnailImg);
         return CommonApiResponse.success(null);
     }
 
