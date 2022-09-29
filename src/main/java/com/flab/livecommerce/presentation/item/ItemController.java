58c319b27c276dd50a1aff22613b4132bca1d2f2
DJ-archive <ydajeong7@gmail.com>
2022-09-29 11:17:17 +0900

    refactor: api 주소 수정
    - 명구님이 shop 구현 후, shopId 추가

--- src/main/java/com/flab/livecommerce/presentation/item/ItemController.java
+++ src/main/java/com/flab/livecommerce/presentation/item/ItemController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-@RequestMapping("/api/v1/store/{storeId}/item")
+@RequestMapping("/api/v1/item")
 @RestController
 public class ItemController {
 
