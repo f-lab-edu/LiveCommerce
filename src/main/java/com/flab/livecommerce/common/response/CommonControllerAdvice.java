ff8bd5f3dc5d68263ab6169a4b878113186a9ca6
Jeong Myeonggu <audrn6689@gmail.com>
2022-10-04 17:15:33 +0900

    refactor: 공통 예외 수정
    에러코드 추가, ExceptionHandler 수정 및 추가, 응답 메서드 추가

--- src/main/java/com/flab/livecommerce/common/response/CommonControllerAdvice.java
+++ src/main/java/com/flab/livecommerce/common/response/CommonControllerAdvice.java
@@ -1,7 +1,9 @@
 package com.flab.livecommerce.common.response;
 
+import com.flab.livecommerce.common.exception.BaseException;
 import com.flab.livecommerce.common.exception.InvalidParameterException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.NestedExceptionUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -11,10 +13,31 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class CommonControllerAdvice {
 
+    /**
+     * http status: 500 AND result: FAIL
+     * 정의되지 않은 Exception 상황
+     * 시스템 예외 상황. 모니터링 대상
+     */
+    @ResponseStatus
+    @ExceptionHandler(Exception.class)
+    public CommonApiResponse onException(Exception e) {
+        log.error("error =", e);
+        return CommonApiResponse.fail(ErrorCode.COMMON_SYSTEM_ERROR);
+    }
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(InvalidParameterException.class)
-    public CommonApiResponse passwordNotMatchedException(InvalidParameterException e) {
-        return CommonApiResponse.fail(e.getErrorCode().getMessage(), e.getMessage());
+    /**
+     * http status: 200 AND result: FAIL
+     * 시스템은 이슈 없고, 비즈니스 로직 처리에서 에러가 발생.
+     * 확장한 예상 된 예외 발생
+     */
+    @ResponseStatus(HttpStatus.OK)
+    @ExceptionHandler(BaseException.class)
+    public CommonApiResponse onBaseException(BaseException e) {
+        log.warn(
+            "cause ={}, errorMsg = {}",
+            NestedExceptionUtils.getMostSpecificCause(e),
+            NestedExceptionUtils.getMostSpecificCause(e).getMessage()
+        );
+        return CommonApiResponse.fail(e.getErrorCode().name(), e.getMessage());
     }
 }
