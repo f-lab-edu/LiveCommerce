df049a9d64e4bb8e99aacfe81e030c1da5ca800d
DJ-archive <ydajeong7@gmail.com>
2022-10-13 18:00:20 +0900

    feat: 아이템 이미지 전달방식 변경

--- src/main/java/com/flab/livecommerce/presentation/item/ItemController.java
+++ src/main/java/com/flab/livecommerce/presentation/item/ItemController.java
@@ -1,9 +1,16 @@
 package com.flab.livecommerce.presentation.item;
 
+import com.flab.livecommerce.application.item.facade.ItemImageManager;
 import com.flab.livecommerce.application.item.facade.ItemManager;
 import com.flab.livecommerce.common.response.CommonApiResponse;
+import com.flab.livecommerce.domain.item.Item;
 import com.flab.livecommerce.presentation.item.request.ItemFormRequest;
+import java.net.URI;
 import javax.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,24 +18,45 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+@Slf4j
 @RequestMapping("/api/v1/item")
 @RestController
 public class ItemController {
 
     private final ItemManager itemManager;
+    private final ItemImageManager itemImageManager;
 
-    public ItemController(ItemManager itemManager) {
+    public ItemController(ItemManager itemManager, ItemImageManager itemImageManager) {
         this.itemManager = itemManager;
+        this.itemImageManager = itemImageManager;
     }
 
-    @PostMapping
-    public CommonApiResponse registerItem(
-        @RequestBody @Valid ItemFormRequest request
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseEntity registerItem(
+        @RequestPart("ItemRegisterRequest") @Valid ItemFormRequest request,
+        @RequestPart("thumbnailImg") MultipartFile thumbnailImage,
+        @RequestPart(value = "specificImg", required = false) MultipartFile[] specificImages
     ) {
-        itemManager.register(request.toCommand());
-        return CommonApiResponse.success(null);
+        Item registeredItem = itemManager.register(request.toCommand());
+
+        URI location = ServletUriComponentsBuilder
+            .fromCurrentRequest()
+            .path("/{id}")
+            .path("/image")
+            .buildAndExpand(registeredItem.getId())
+            .toUri();
+        log.info("location:{}", location);
+
+        //itemImageManager.uploadItemImage(thumbnailImage, specificImages);
+
+        return ResponseEntity.created(location).build();
     }
 
     @GetMapping("/{itemId}")
--- src/main/resources/db/migration/V1.1__alter_item_option_table_add_fk.sql
+++ src/main/resources/db/migration/V1.1__alter_item_option_table_add_fk.sql
@@ -2,3 +2,4 @@ ALTER TABLE item_option_group ADD FOREIGN KEY(item_id) REFERENCES item(id);
 
 ALTER TABLE item_option ADD FOREIGN KEY(item_option_group_id) REFERENCES item_option_group(id);
 
+ALTER TABLE item DROP COLUMN model_number;
