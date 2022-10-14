f7ef22d5c779e2d92d2981e9e378cc0f721ee4fe
Jeong Myeonggu <audrn6689@gmail.com>
2022-10-14 18:42:45 +0900

    chore: mybatis Repository 임시 커밋

---
+++ src/main/java/com/flab/livecommerce/infrastructure/order/persistence/mybatis/MyBatisOrderRepository.java
@@ -0,0 +1,10 @@
+package com.flab.livecommerce.infrastructure.order.persistence.mybatis;
+
+import com.flab.livecommerce.domain.order.Order;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface MyBatisOrderRepository {
+
+    Order save(Order order);
+}
