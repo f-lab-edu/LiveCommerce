6f9b67dc9cf3eb8daf375bb7a06cd857e6e2df11
Jeong Myeonggu <audrn6689@gmail.com>
2022-10-20 20:00:57 +0900

    refactor: 에러코드 추가

--- src/main/java/com/flab/livecommerce/common/response/ErrorCode.java
+++ src/main/java/com/flab/livecommerce/common/response/ErrorCode.java
@@ -3,9 +3,19 @@ package com.flab.livecommerce.common.response;
 
 public enum ErrorCode {
 
+
     COMMON_SYSTEM_ERROR("오류가 발생했습니다. 잠시 후 다시 시도해주세요"), //장애 상황
     COMMON_INVALID_PARAMETER("요청한 값이 올바르지 않습니다."),
-    COMMON_ILLEGAL_STATUS("잘못된 상태값입니다.");
+    COMMON_ILLEGAL_STATUS("잘못된 상태값입니다."),
+
+    // User Error
+    INVALID_USER("유효하지 않은 회원입니다."),
+    DUPLICATE_EMAIL("중복된 이메일 입니다."),
+    PASSWORD_NOT_MATCHED("비밀번호가 일치하지 않습니다."),
+
+    INVALID_TOKEN("유효하지 않은 토큰입니다.");
+
+
 
     private final String message;
 
