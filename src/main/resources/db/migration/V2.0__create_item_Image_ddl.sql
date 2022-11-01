48fd185111cd82fe75d9d467e97ae842215eb44e
DJ-archive <ydajeong7@gmail.com>
2022-11-01 21:26:11 +0900

    refactor: 리뷰 반영
    - 한 피처 안에서 변경되는 flyway 스키마 통합

--- src/main/resources/db/migration/V2.0__create_item_Image_ddl.sql
+++ src/main/resources/db/migration/V2.0__create_item_Image_ddl.sql
@@ -1,7 +1,9 @@
 CREATE TABLE item_image
 (
-    id      BIGINT       not null auto_increment primary key comment 'ID',
-    name    VARCHAR(300) NOT NULL,
-    url     VARCHAR(300) NOT NULL,
-    item_id INT          NOT NULL
+    id      BIGINT        not null auto_increment primary key comment 'ID',
+    item_id bigint not null comment '상품 ID',
+    name    VARCHAR(500) NOT NULL comment '이미지 파일 이름',
+    url     VARCHAR(2500) NOT NULL comment '이미지 주소',
+    ordering tinyint(1) comment '정렬 순서',
+    FOREIGN KEY(item_id) REFERENCES item(id)
 );
--- src/main/resources/db/migration/V2.1__alter_table_item_image.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE item_image
-    MODIFY item_id bigint not null comment '상품 ID',
-    MODIFY url VARCHAR(2500) comment '이미지 주소',
-    MODIFY name VARCHAR(500) comment '이미지 파일 이름',
-    ADD COLUMN ordering tinyint(1) comment '정렬 순서';
-
-ALTER TABLE item_image ADD FOREIGN KEY(item_id) REFERENCES item(id);
