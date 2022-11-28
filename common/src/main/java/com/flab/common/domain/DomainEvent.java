1e55849c20669e8ae2b33f1a56fba32337dbfed1
Jeong Myeonggu <audrn6689@gmail.com>
2022-11-28 20:01:54 +0900

    fix: 체크스타일 오류 해결
    - 첫 문장에 . 찍기

--- common/src/main/java/com/flab/common/domain/DomainEvent.java
+++ common/src/main/java/com/flab/common/domain/DomainEvent.java
@@ -3,7 +3,7 @@ package com.flab.common.domain;
 import java.time.LocalDateTime;
 
 /**
- * 모든 이벤트에서 구현되는 최소한의 인터페이스
+ * 모든 이벤트에서 구현되는 최소한의 인터페이스.
  * 모든 이벤트의 기본 계약을 강화 - iddd 382p
  */
 public interface DomainEvent {
