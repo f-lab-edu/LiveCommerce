e4de829d61cb2cc9943c68eb8951988ddaf03dd7
Jeong Myeonggu <audrn6689@gmail.com>
2022-12-18 17:48:32 +0900

    refactor: 결제 이벤트에 주문항목 정보 포함하도록 수정

--- order-api/order-domain/src/main/java/com/flab/order/domain/event/OrderPayedEvent.java
+++ order-api/order-domain/src/main/java/com/flab/order/domain/event/OrderPayedEvent.java
@@ -2,16 +2,21 @@ package com.flab.order.domain.event;
 
 import com.flab.common.domain.DomainEvent;
 import com.flab.order.domain.Order;
+import com.flab.order.domain.OrderLineItem;
 import java.time.LocalDateTime;
+import java.util.List;
 
 public final class OrderPayedEvent implements DomainEvent {
+
     private final Long orderId;
     private final Long userId;
+    private final List<OrderLineItem> orderLineItems;
     private final LocalDateTime occurredOn;
 
     public OrderPayedEvent(Order order) {
         this.orderId = order.getId();
         this.userId = order.getUserId();
+        this.orderLineItems = order.getOrderLineItems();
         this.occurredOn = LocalDateTime.now();
     }
 
