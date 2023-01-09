3dd1090f37681fd5244a45a896b767f5624b853c
Jeong Myeonggu <audrn6689@gmail.com>
2023-01-09 17:34:10 +0900

    refactor: 재고도메인측에서 정렬하는 책임을 갖도록 로직 수정

--- inventory-api/inventory-presentation/src/main/java/com/flab/inventory/presentation/request/OrderPayedEvent.java
+++ inventory-api/inventory-presentation/src/main/java/com/flab/inventory/presentation/request/OrderPayedEvent.java
@@ -1,11 +1,14 @@
 package com.flab.inventory.presentation.request;
 
 
+import static java.util.Comparator.comparing;
+
 import com.flab.common.domain.DomainEvent;
 import com.flab.inventory.application.command.OrderPayedCommand;
 import com.flab.inventory.domain.ItemQuantity;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public final class OrderPayedEvent implements DomainEvent {
 
@@ -28,7 +31,13 @@ public final class OrderPayedEvent implements DomainEvent {
     }
 
     public OrderPayedCommand toCommand() {
-        return new OrderPayedCommand(itemQuantities);
+        return new OrderPayedCommand(sortedItemQuantities());
+    }
+
+    private List<ItemQuantity> sortedItemQuantities() {
+        return this.itemQuantities.stream()
+            .sorted(comparing(ItemQuantity::getItemId))
+            .collect(Collectors.toList());
     }
 
     public Long getOrderId() {
