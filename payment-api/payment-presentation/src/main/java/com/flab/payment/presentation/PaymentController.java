57cfa5aef3d5cdf8f9818783429c3eaa3ad67790
Jeong Myeonggu <audrn6689@gmail.com>
2023-03-09 21:57:11 +0900

    refactor: URL 표현을 단수가 아닌 복수로 변경
    - 복수의 표현이 좀 더 리소스를 표현하기 적합하다고 생각

--- payment-api/payment-presentation/src/main/java/com/flab/payment/presentation/PaymentController.java
+++ payment-api/payment-presentation/src/main/java/com/flab/payment/presentation/PaymentController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/api/v1/payment")
+@RequestMapping("/api/v1/payments")
 @RestController
 public class PaymentController {
 
--- user-api/user-presentation/src/main/java/com/flab/user/presentation/UserController.java
+++ user-api/user-presentation/src/main/java/com/flab/user/presentation/UserController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/api/v1/user")
+@RequestMapping("/api/v1/users")
 @RestController
 public class UserController {
 
