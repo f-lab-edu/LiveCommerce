395de8cf2144b5701f57dcf1493bdec08c1bbbd0
Jeong Myeonggu <audrn6689@gmail.com>
2023-03-10 22:51:06 +0900

    refactor & feat: 가독성 및 에러코드 수정 작업, 유저 모듈 에러핸들러 추가
    - ErrorCode 추가 (추후 코드명을 좀 깔끔하기 다듬기)
    - UserExceptionHandler 추가, 도메인에서 발생한 예외는 해당 도메인이 처리하여 응답한다.
    - controller 가독성 향상

--- common/src/main/java/com/flab/common/exception/ErrorCode.java
+++ common/src/main/java/com/flab/common/exception/ErrorCode.java
@@ -7,6 +7,8 @@ public enum ErrorCode {
     COMMON_SYSTEM_ERROR("오류가 발생했습니다. 잠시 후 다시 시도해주세요"), //장애 상황
     COMMON_INVALID_PARAMETER("요청한 값이 올바르지 않습니다."),
     COMMON_ILLEGAL_STATUS("잘못된 상태값입니다."),
+    COMMON_METHOD_NOT_ALLOWED("지원하지 않는 HTTP METHOD 입니다."),
+    COMMON_UNSUPPORTED_MEDIA_TYPE("지원하지 않는 MEDIA TYPE 입니다."),
 
     UN_AUTHENTICATED("인증되지 않은 접근입니다."),
     UNAUTHORIZED_ROLE("현재 유저 권한으로는 접근할 수 없는 리소스 요청입니다."),
@@ -15,21 +17,21 @@ public enum ErrorCode {
     USER_NOT_FOUND("회원을 찾을 수 없습니다."),
     INVALID_USER("유효하지 않은 회원입니다."),
     INVALID_TOKEN("유효하지 않은 인증 토큰입니다."),
+    DUPLICATED_EMAIL("중복된 이메일 입니다."),
+    PASSWORD_NOT_MATCHED("비밀번호가 일치하지 않습니다."),
 
 
     // Seller Error
     SELLER_NOT_FOUND("판매자를 찾을 수 없습니다."),
     INVALID_SELLER("유효하지 않은 판매자입니다."),
 
-    // User, Seller 공통
-    DUPLICATED_EMAIL("중복된 이메일 입니다."),
-    PASSWORD_NOT_MATCHED("비밀번호가 일치하지 않습니다."),
 
     //Order Error
     ALREADY_CANCELED("이미 취소된 주문입니다."),
     ALREADY_PAYED("이미 결제된 주문입니다."),
     ALREADY_COMPLETED("이미 완료된 주문입니다."),
     NOT_MATCHED_AMOUNT("결제 금액과 주문 금액이 일치하지 않습니다."),
+    ORDER_NOT_FOUND("해당 주문을 찾을 수 없습니다."),
 
     //Inventory Error
     NOT_ENOUGH_QUANTITY("재고 수량이 충분하지 않습니다."),
