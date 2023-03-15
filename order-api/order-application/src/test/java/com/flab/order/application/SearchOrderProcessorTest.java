bea8cbba90e24099d5d1a4f26f4ca62be0379e2f
Jeong Myeonggu <audrn6689@gmail.com>
2023-03-15 22:43:44 +0900

    test: Order 도메인 테스트코드 리팩토링 (단위테스트 책 참고)
    - 중복코드 최대한 제거
    - 테스트 실행 메서드 이름 변경(DisplayName 이 있기 때문)
    - PaymentCompletedProcessorTest 는 sut가 제대로 정의가 되지 않아 어떻게 더 개선시킬지 알아봐야겠다.

--- order-api/order-application/src/test/java/com/flab/order/application/SearchOrderProcessorTest.java
+++ order-api/order-application/src/test/java/com/flab/order/application/SearchOrderProcessorTest.java
@@ -5,28 +5,29 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchThrowable;
 
 import com.flab.common.exception.EntityNotFoundException;
-import com.flab.order.application.command.CreateOrderCommand;
-import com.flab.order.application.command.CreateOrderItemOptionCommand;
-import com.flab.order.application.command.CreateOrderItemOptionGroupCommand;
-import com.flab.order.application.command.CreateOrderLineItemCommand;
 import com.flab.order.application.result.OrderResult;
 import com.flab.order.domain.Order;
+import com.flab.order.domain.OrderItemOption;
+import com.flab.order.domain.OrderItemOptionGroup;
+import com.flab.order.domain.OrderLineItem;
+import com.flab.order.domain.OrderRepository;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class SearchOrderProcessorTest {
 
+    private final OrderRepository orderRepository = new FakeOrderRepository();
+
     @Test
     @DisplayName("주문을 찾을 수 없는 경우 예외가 발생한다.")
-    void searchOrder_NotFound() {
+    void test1() {
         //Arrange
-        var orderRepository = new FakeOrderRepository();
-
-        var processor = new SearchOrderProcessor(orderRepository);
+        var sut = new SearchOrderProcessor(orderRepository);
+        Long command = 2L;
 
         //Act
-        Throwable result = catchThrowable(() -> processor.execute(2L));
+        Throwable result = catchThrowable(() -> sut.execute(command));
 
         //Assert
         assertThat(result.getClass()).isEqualTo(EntityNotFoundException.class);
@@ -34,17 +35,16 @@ class SearchOrderProcessorTest {
 
     @Test
     @DisplayName("주문을 찾으면 주문을 반환한다.")
-    void searchOrder_Found() {
+    void test2() {
         //Arrange
-        var orderRepository = new FakeOrderRepository();
-        var order = Order.create(1L, command().getPayMethod(), command().toLineItems());
-        order.setId(1L);
+        Order order = orderCreate();
+        Long command = 1L;
         orderRepository.save(order);
 
-        var processor = new SearchOrderProcessor(orderRepository);
+        var sut = new SearchOrderProcessor(orderRepository);
 
         //Act
-        OrderResult result = processor.execute(1L);
+        OrderResult result = sut.execute(command);
 
         //Assert
         assertThat(result.getOrderId()).isEqualTo(order.getId());
@@ -52,54 +52,28 @@ class SearchOrderProcessorTest {
         assertThat(result.getTotalAmount()).isEqualTo(order.getTotalAmount());
     }
 
-    private CreateOrderCommand command() {
-        return new CreateOrderCommand(
-            "NAVER_PAY",
-            List.of(new CreateOrderLineItemCommand(
+    private Order orderCreate() {
+        return Order.create(1L, "NAVER_PAY",
+            List.of(new OrderLineItem(
                     3,
                     1L,
                     "티셔츠",
                     30000,
-                    List.of(new CreateOrderItemOptionGroupCommand(
+                    List.of(new OrderItemOptionGroup(
                             1,
                             "사이즈",
-                            List.of(new CreateOrderItemOptionCommand(
+                            List.of(new OrderItemOption(
                                     1,
                                     "MEDIUM",
                                     1000
                                 )
                             )
-                        ), new CreateOrderItemOptionGroupCommand(
+                        ), new OrderItemOptionGroup(
                             2,
                             "컬러",
-                            List.of(new CreateOrderItemOptionCommand(
+                            List.of(new OrderItemOption(
                                     1,
                                     "BLACK",
-                                    2000
-                                )
-                            )
-                        )
-                    )
-                ), new CreateOrderLineItemCommand(
-                    1,
-                    1L,
-                    "양말",
-                    2000,
-                    List.of(new CreateOrderItemOptionGroupCommand(
-                            1,
-                            "사이즈",
-                            List.of(new CreateOrderItemOptionCommand(
-                                    1,
-                                    "LARGE",
-                                    0
-                                )
-                            )
-                        ), new CreateOrderItemOptionGroupCommand(
-                            2,
-                            "컬러",
-                            List.of(new CreateOrderItemOptionCommand(
-                                    1,
-                                    "RED",
                                     2000
                                 )
                             )
